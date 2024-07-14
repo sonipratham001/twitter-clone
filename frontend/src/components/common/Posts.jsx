b831c40c46b78de5cb2f986b5c9e1a89df57d5fd
sonipratham001 <sonipratham3008@gmail.com>
2024-07-14 16:11:41 +0530

    3-Profile page posts and user info fetched

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -3,13 +3,17 @@ import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton.jsx";
 import {useQuery} from "@tanstack/react-query";
 import { useEffect } from "react";
-const Posts = ({feedType}) => {
+const Posts = ({feedType, username, userId}) => {
    const getPostEndPoint = () =>{
 	 switch (feedType) {
 		case "forYou":
 			return "/api/posts/all";
 		case "following":
 			return "/api/posts/following";
+		case "posts":
+			return `/api/posts/user/${username}`;
+		case "likes":
+			return 	`/api/posts/likes/${userId}`;	
 		default:
 			return "/api/posts/all";	
 	 }

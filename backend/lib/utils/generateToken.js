309ec5a88c38dca47b5c991b8c5de2212bc7b805
sonipratham001 <sonipratham3008@gmail.com>
2024-07-15 11:46:24 +0530

    final errors resolved

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -1,12 +1,14 @@
-export const generateTokenAndSetCookie = (userId, res) => {
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-        expiresIn: '30d' // For example, update to 30 days
-    });
+import jwt from "jsonwebtoken";
 
-    res.cookie("jwt", token, {
-        maxAge: 30 * 24 * 60 * 60 * 1000, // Update cookie expiration to match the new token expiration
-        httpOnly: true,
-        sameSite: "strict",
-        secure: process.env.NODE_ENV !== "development",
-    });
+export const generateTokenAndSetCookie = (userId, res) => {
+	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+		expiresIn: "15d",
+	});
+
+	res.cookie("jwt", token, {
+		maxAge: 15 * 24 * 60 * 60 * 1000, //MS
+		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+		sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+		secure: process.env.NODE_ENV !== "development",
+	});
 };
